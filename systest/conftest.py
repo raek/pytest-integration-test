7c14c6967f77d15d5e3227faf5efa34a76c8eb83
Rasmus Bondesson <raek@raek.se>
2020-09-30 17:56:02 +0200

    Resutructure code: separate framework from tests

--- systest/conftest.py
+++ systest/conftest.py
@@ -1,10 +1,16 @@
 import pytest
 
 
+def pytest_configure(config):
+    config.addinivalue_line("usefixtures", "charging_cable")
+    config.addinivalue_line("markers", "initialize")
+    config.addinivalue_line("markers", "features")
+
+
 @pytest.fixture
 def debug_port():
     """Line-based access to the main debug port"""
-    from debug_port import DebugPort
+    from fw.debug_port import DebugPort
     with DebugPort("/dev/ttyUSB0", 115200) as dp:
         yield dp
 
@@ -16,7 +22,7 @@ def charging_cable():
     When tests are not running the charging cable should be left connected, so
     that the battery does not drain.
     """
-    from cable_control import ChargingCable
+    from fw.cable_control import ChargingCable
     cc = ChargingCable()
     yield cc
     cc.connect()
@@ -25,7 +31,7 @@ def charging_cable():
 @pytest.fixture
 def power_cycled(debug_port, charging_cable):
     """Make sure DUT is freshly restarted at beginning of test"""
-    from bootup import bootup
+    from fw.bootup import bootup
     with debug_port.listen() as lines:
         with charging_cable.temporarily_disconnected():
             debug_port.toggle_dtr()
@@ -35,7 +41,7 @@ def power_cycled(debug_port, charging_cable):
 @pytest.fixture
 def command_runner(debug_port):
     """Run commands on the DUT"""
-    from command_runner import CommandRunner
+    from fw.command_runner import CommandRunner
     cr = CommandRunner(debug_port)
     assert cr.run_command("ping") == ["pong"]
     return cr
