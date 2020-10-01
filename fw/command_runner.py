4998615357e5c7543164d8fff45498344a4caabd
Rasmus Bondesson <raek@raek.se>
2020-10-01 17:29:10 +0200

    Add timeout option to run_command too

--- fw/command_runner.py
+++ fw/command_runner.py
@@ -1,25 +1,29 @@
 from fw.timeout import TimeoutCalculator
 
 
+DEFAULT_COMMAND_TIMEOUT_SECONDS = 20
+
+
 class CommandError(Exception):
     pass
+
 
 class CommandRunner:
     def __init__(self, debug_port):
         self._debug_port = debug_port
 
-    def run_command(self, command):
+    def run_command(self, command, timeout_seconds=DEFAULT_COMMAND_TIMEOUT_SECONDS):
         """Send a command, collect its output lines and check that it succeeded.
 
         Returns a list of lines
         """
-        ok, lines = self.try_run_command(command)
+        ok, lines = self.try_run_command(command, timeout_seconds)
         if ok:
             return lines
         else:
             raise CommandError("Error running command: " + command)
 
-    def try_run_command(self, command, timeout_seconds=20):
+    def try_run_command(self, command, timeout_seconds=DEFAULT_COMMAND_TIMEOUT_SECONDS):
         """Send a command and return whether it succeeded and the lines output by the command.
 
         Returns a (bool, lines) pair.
